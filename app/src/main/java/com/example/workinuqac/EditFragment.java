38479f2c85bace56615fb1a82210c241ce985739
Clémence <clemence.ptii@gmail.com>
2020-04-30 10:36:38 +0200

    editer ses informations - step 5

--- app/src/main/java/com/example/workinuqac/EditFragment.java
+++ app/src/main/java/com/example/workinuqac/EditFragment.java
@@ -51,8 +51,8 @@ public class EditFragment extends Fragment {
         LinearLayout nameLayout=getView().findViewById(R.id.nameLayout);
         final View nameView = inflater.inflate(R.layout.information_edit_profile_layout,null);
         nameView.setTag("nameLayout");
-        Button editNameButton = nameView.findViewById(R.id.infomationEditButton);
-        editNameButton.setOnClickListener(new Button.OnClickListener() {
+        Button editButton = nameView.findViewById(R.id.infomationEditButton);
+        editButton.setOnClickListener(new Button.OnClickListener() {
             public void onClick(View v) {
                 goToEdit(v);
             }
@@ -62,8 +62,8 @@ public class EditFragment extends Fragment {
         LinearLayout mailLayout=getView().findViewById(R.id.mailLayout);
         final View mailView = inflater.inflate(R.layout.information_edit_profile_layout,null);
         mailView.setTag("mailLayout");
-        editNameButton = mailView.findViewById(R.id.infomationEditButton);
-        editNameButton.setOnClickListener(new Button.OnClickListener() {
+        editButton = mailView.findViewById(R.id.infomationEditButton);
+        editButton.setOnClickListener(new Button.OnClickListener() {
             public void onClick(View v) {
                 goToEdit(v);
             }
@@ -73,13 +73,24 @@ public class EditFragment extends Fragment {
         LinearLayout statusLayout=getView().findViewById(R.id.statusLayout);
         final View statusView = inflater.inflate(R.layout.information_edit_profile_layout,null);
         statusView.setTag("statusLayout");
-        editNameButton = statusView.findViewById(R.id.infomationEditButton);
-        editNameButton.setOnClickListener(new Button.OnClickListener() {
+        editButton = statusView.findViewById(R.id.infomationEditButton);
+        editButton.setOnClickListener(new Button.OnClickListener() {
             public void onClick(View v) {
                 goToEdit(v);
             }
         });
         statusLayout.addView(statusView);
+
+        LinearLayout mdpLayout=getView().findViewById(R.id.mdpLayout);
+        final View mdpView = inflater.inflate(R.layout.mdp_info_edit_profile_layout,null);
+        mdpView.setTag("mdpLayout");
+        editButton = mdpView.findViewById(R.id.infomationEditButton);
+        editButton.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                goToMdpEdit(v);
+            }
+        });
+        mdpLayout.addView(mdpView);
 
 
 
@@ -110,6 +121,34 @@ public class EditFragment extends Fragment {
         parentlayout.addView(rowView);
         Toast.makeText(getContext(), parentlayout.getId()+" id parent", Toast.LENGTH_SHORT).show();
     }
+    private void goToMdpEdit(View v){
+        Toast.makeText(getContext(), "editer cette information "+v.getTag()+" "+getId(), Toast.LENGTH_SHORT).show();
+        String parentTag=((View)v.getParent()).getTag().toString();
+        LinearLayout parentlayout=getView().findViewWithTag(parentTag);
+        parentlayout.removeAllViews();
+        LayoutInflater inflater=(LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        final View rowView = inflater.inflate(R.layout.mdp_editing_edit_profile_layout,null);
+        rowView.setTag(parentTag);
+        Button validateButton = rowView.findViewById(R.id.editingvalidateButton);
+        validateButton.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                if(checkMdp()){
+                    validateInformation();
+                    mdpBackInformation(v);
+                }
+
+            }
+        });
+        Button cancelButton = rowView.findViewById(R.id.editingcancelButton);
+        cancelButton.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                cancelInformation();
+                mdpBackInformation(v);
+            }
+        });
+        parentlayout.addView(rowView);
+        Toast.makeText(getContext(), parentlayout.getId()+" id parent", Toast.LENGTH_SHORT).show();
+    }
 
     private void BackInformation(View v){
         Toast.makeText(getContext(), "back information", Toast.LENGTH_SHORT).show();
@@ -127,6 +166,28 @@ public class EditFragment extends Fragment {
         });
         parentlayout.addView(rowView);
 
+    }
+    private void mdpBackInformation(View v){
+        //Toast.makeText(getContext(), "back information", Toast.LENGTH_SHORT).show();
+        String parentTag=((View)((View)v.getParent()).getParent()).getTag().toString();
+        Toast.makeText(getContext(), parentTag, Toast.LENGTH_SHORT).show();
+        LinearLayout parentlayout=getView().findViewWithTag(parentTag);
+        parentlayout.removeAllViews();
+        LayoutInflater inflater=(LayoutInflater)getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        final View rowView = inflater.inflate(R.layout.mdp_info_edit_profile_layout,null);
+        rowView.setTag(parentTag);
+        Button editNameButton = rowView.findViewById(R.id.infomationEditButton);
+        editNameButton.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                goToMdpEdit(v);
+            }
+        });
+        parentlayout.addView(rowView);
+
+    }
+
+    private boolean checkMdp(){
+        return false;
     }
 
     private void cancelInformation(){
