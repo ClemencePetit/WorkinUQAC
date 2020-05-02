f34c8368add00415ab4d594a927b40e2f752f248
LadyCookie <bouzerea@etud.insa-toulouse.fr>
2020-05-02 19:41:35 -0400

    Ajout/update et lecture des users dans la BDD

--- app/src/main/java/com/example/workinuqac/MyBDD.java
+++ app/src/main/java/com/example/workinuqac/MyBDD.java
@@ -1,5 +1,6 @@
 package com.example.workinuqac;
 
+import android.media.browse.MediaBrowser;
 import android.util.Log;
 
 import androidx.annotation.NonNull;
@@ -16,24 +17,82 @@ import java.util.List;
 
 public class MyBDD {
 
-    static public void writeNewUser(String email,String name, List<String> cours){
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("users");
-        //on rajoute ou update le nom associé à l'email
-        myRef.child(email).child("name").setValue(name);
-        myRef.child(email).child("courses").setValue(cours);
+    static private String currentEmail;
+    static private String currentUsername;
+    static private ArrayList<String> currentCoursesList;
+
+    //GETTERS
+    static public String getCurrentEmail(){
+        return currentEmail;
     }
 
-    static public void readUser(String email){
+    static public String getCurrentUsername(){
+        return currentUsername;
+    }
+
+    static public ArrayList<String> getCurrentCoursesList(){
+        return currentCoursesList;
+    }
+
+    //WRITING IN DB METHODS
+    static public void writeNewUser(String codePermanent, String email,String name, List<String> cours){
+
         FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference nameRef = database.getReference("users/"+email+"/name");
-        DatabaseReference coursRef = database.getReference("users/"+email+"courses");
-        //Adding Listener on name
+        DatabaseReference myRef = database.getReference("users");
+        //on rajoute ou update le nom associé au code permanent
+
+        myRef.child(codePermanent).child("name").setValue(name);
+        myRef.child(codePermanent).child("courses").setValue(cours);
+        myRef.child(codePermanent).child("email").setValue(email);
+
+        Log.d("BDD","User updated");
+    }
+
+    static public void updateUserName(String codePermanent, String Username){
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("users");
+        //on rajoute ou update le nom associé au code permanent
+
+        myRef.child(codePermanent).child("name").setValue(Username);
+
+        Log.d("BDD","User updated");
+    }
+
+    static public void updateEmail(String codePermanent, String Email){
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("users");
+        //on rajoute ou update le nom associé au code permanent
+        myRef.child(codePermanent).child("email").setValue(Email);
+
+        Log.d("BDD","User updated");
+    }
+
+    static public void updateCourses(String codePermanent, List<String> cours){
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("users");
+        //on rajoute ou update le nom associé au code permanent
+        myRef.child(codePermanent).child("courses").setValue(cours);
+
+        Log.d("BDD","User updated");
+    }
+
+    //READING DB METHODS
+    public interface OnDataReadEventListener{
+        public void onEvent();
+    }
+
+    //read the username of the user with codePermanent. Result stored in currentUsername;Execute oc when data loaded
+    static public void readUserName(String codePermanent, final OnDataReadEventListener oc){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference nameRef = database.getReference("users/"+codePermanent+"/name");//Adding Listener on name
         ValueEventListener nameListener = new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                String name = dataSnapshot.getValue(String.class);
-                Log.d("BDD","Username : " + name);
+               currentUsername = dataSnapshot.getValue(String.class);
+                oc.onEvent();
             }
 
             @Override
@@ -42,14 +101,41 @@ public class MyBDD {
             }
         };
         nameRef.addListenerForSingleValueEvent(nameListener);
+    }
+
+    //read the email of the user with codePermanent. Result stored in currentEmail. Execute oc when data loaded
+    static public void readUserEmail(String codePermanent, final OnDataReadEventListener oc){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference emailRef = database.getReference("users/"+codePermanent+"/email");
+        //Adding Listener on email
+        ValueEventListener emailListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                currentEmail = dataSnapshot.getValue(String.class);
+                oc.onEvent();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.w("BDD", "loadName:onCancelled",databaseError.toException());
+            }
+        };
+        emailRef.addListenerForSingleValueEvent(emailListener);
+    }
+
+    //read the courses of the user with codePermanent. Result stored in currentCoursesList. Execute oc when data loaded
+    static public void readUserCourses(String codePermanent, final OnDataReadEventListener oc){
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference coursRef = database.getReference("users/"+codePermanent+"/courses");
         //Adding Listener on courses
         coursRef.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                currentCoursesList = new ArrayList<String>();
                 for(DataSnapshot postSnapshot : dataSnapshot.getChildren()){
-                    // TODO
-                    Log.d("BDD","Username cours " + postSnapshot.toString());
+                    currentCoursesList.add(postSnapshot.getValue().toString());
                 }
+                oc.onEvent();
             }
 
             @Override
